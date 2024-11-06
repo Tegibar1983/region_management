d142154718e3e8ee1839efed1633f1062c7e82fb
Tegibar1983 <tbtf1991@gmail.com>
2024-11-06 16:12:06 +0300

    Create Models & Make Migrations

--- ethio_regions/regional_info/models.py
+++ ethio_regions/regional_info/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class RegionalInfo(models.Model):
+    region_number=models.PositiveSmallIntegerField()
+    region_name=models.CharField(max_length=200)
+    population_size=models.PositiveBigIntegerField()
+    area=models.FloatField()
+    capital_city=models.CharField(max_length=200)
+    flag=models.ImageField(upload_to='flag')
+    email=models.EmailField()
+    map=models.ImageField(upload_to='map')
+    website=models.URLField()
+    
